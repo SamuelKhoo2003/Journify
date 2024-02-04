171c5cb66bc183c656c237ee51da162d20d82396
adagio7 <haojiepe@gmail.com>
2024-02-04 06:08:16 +0000

    chore: updated insights

--- src/app/insights.tsx
+++ src/app/insights.tsx
@@ -10,11 +10,11 @@ const Insights = () => {
             <div id="notlast" className="section"></div>
             <h2 className="text-4xl">Your Insights</h2>
             <h3 className="text-2xl happiness-info">
-                You were <span className="text-2xl text-green-500 font-extrabold">happy</span> on average last year.
-                 with an average score of <span className="text-2xl font-extrabold">{formatToTwoDecimals(averageSentimentForPastYear())} </span>
-                 over <span className="text-2xl font-extrabold">{formatToTwoDecimals(countJournalEntriesLastYear())}</span> entries!
-                 <br></br><br></br>
-                 Our models predict a happiness value of <span className="text-2xl font-extrabold">{predictedSentiment}</span> next month. {regressionResult > 0 ? "Looks good!" : "Take it easy!"}
+                {/* <span className="text-2xl font-extrabold">{formatToTwoDecimals(averageSentimentForPastYear())} </span> */}
+                You were <span className="text-2xl text-green-500 font-extrabold">happy</span> on average last year
+                over <span className="text-2xl font-extrabold">{formatToTwoDecimals(countJournalEntriesLastYear())}</span> entries!
+                <br></br><br></br>
+                Our models predict a happiness value of <span className="text-2xl font-extrabold">{predictedSentiment}</span> next month. {regressionResult > 0 ? "Looks good!" : "Take it easy!"}
             </h3>
             {/* <ColorChanger /> */}
             <OldJournals/>
